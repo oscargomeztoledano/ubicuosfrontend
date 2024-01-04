4052d6694d14572dd155e9060f903e44552fca3e
Óscar Gómez Toledano <oscar.gomez9@alu.uclm.es>
2024-01-05 00:00:01 +0100

    Gráfica de acusticas hecha

--- src/components/Lists/AcusticaList.jsx
+++ src/components/Lists/AcusticaList.jsx
@@ -3,6 +3,31 @@ import { Row, Col, Container, Badge, CardTitle } from "reactstrap";
 import { getAllContaminacionAcustica } from "../../utils/apicalls";
 import Header from "../blog/Header"
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Bar } from 'react-chartjs-2';
+
+import{
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from 'chart.js'
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+)
 
 export default function AcusticaList() {
     const [acusticas, setAcusticas] = useState(null);
@@ -19,7 +44,25 @@ export default function AcusticaList() {
        getacusticas();
     }, []);
     const defaultTheme = createTheme();
-    return acusticas === null?(
+  
+    const getMaxNMTByName = () => {
+      const nameNMTMap = new Map();
+
+      acusticas.forEach((acustica) => {
+        const name = acustica.Nombre;
+        const nmt = acustica.NMT;
+
+        if (!nameNMTMap.has(name) || nmt > nameNMTMap.get(name)) {
+          nameNMTMap.set(name, nmt);
+        }
+      });
+
+      return Array.from(nameNMTMap.entries()).map(([name, maxNMT]) => ({ name, maxNMT }));
+    };
+
+    const maxNMTByName = getMaxNMTByName();
+
+  return acusticas === null?(
        <ThemeProvider theme={defaultTheme}>
         <Container maxWidth="lg">
         <Header/>
@@ -34,11 +77,27 @@ export default function AcusticaList() {
       <Container maxWidth="lg">
       <Header/>
       <main>
-        <h1>INFORMACION SOBRE LA Contaminacion Acustica</h1> 
+        <h1>Contaminacion Acustica</h1> 
+
+        {/** Gráfico de linea temporal */}
+        <Bar
+            data={{
+              labels: maxNMTByName.map((item)=>item.name),
+              datasets: [
+                {
+                  label: 'NMT (Nivel de Ruido Máximo)',
+                  data: maxNMTByName.map((item)=>item.maxNMT),  
+                  backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                  borderColor: 'rgba(255, 99, 132, 1)',
+                  borderWidth: 1,
+                },
+              ],
+            }}
+          />
 
       </main>
       </Container>
       
       </ThemeProvider>
     );
-    }
+  }
